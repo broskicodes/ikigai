8e3315351b1000182025903c78ba82e017598b6b
Braeden Hall <braeden@brhall.dev>
2024-09-05 14:20:47 +0100

    auth context

---
+++ src/app/template.tsx
@@ -0,0 +1,10 @@
+import { AuthProvider } from "@/providers/authProvider";
+import { PropsWithChildren } from "react";
+
+export default function Template({ children }: PropsWithChildren) {
+    return (
+        <AuthProvider>
+            {children}
+        </AuthProvider>
+    )
+}
---
+++ src/providers/authProvider.tsx
@@ -0,0 +1,36 @@
+import { createContext, useEffect, useMemo, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+interface AuthProviderContext {
+    userId: string | null;
+}
+
+const AuthContext = createContext<AuthProviderContext>({
+    userId: null,
+});
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [userId, setUserId] = useState<string | null>(null);
+
+    const value = useMemo(() => ({ userId }), [userId]);
+
+    useEffect(() => {
+      const storedUserId = localStorage.getItem('userId');
+  
+      if (!storedUserId) {
+        const newUserId = uuidv4();
+        setUserId(newUserId);
+        localStorage.setItem('userId', newUserId);
+      }
+    }, []);
+
+    useEffect(() => {
+        console.log(userId);
+    }, [userId]);
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
