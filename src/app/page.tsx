6adca4b6a168da7b80283d49a76f0dad49625ca0
Braeden Hall <braeden@brhall.dev>
2024-09-09 16:11:36 +0100

    basic lp styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-"use client";
-
 import { Footer } from "@/components/lp/footer";
 import { Header } from "@/components/lp/header";
 import { Hero } from "@/components/lp/hero";
 import { Testimonials } from "@/components/lp/testimonials";
+
 
 export default function Home() {
   return (
